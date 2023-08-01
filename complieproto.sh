72b5ebd1168d4560544968c52d0d2d7965af76e7
Shubh@m_hack <shubhamtiwaricr07@gmail.com>
2023-08-01 21:26:42 +0530

    code commit

--- complieproto.sh
+++ complieproto.sh
@@ -9,22 +9,46 @@ echo "==========================================================================
 echo "************************************ Protobuf Compilation ****************************************"
 echo "=================================================================================================="
 echo [DELETING..........]
-rm -rf studence-util/src/main/proto
-rm -rf studence-common/src/main/proto
-rm -rf studence-database/src/main/proto
-rm -rf studence-common-http/src/main/proto
-rm -rf studence-organisation/src/main/proto
-rm -rf studence-campus/src/main/proto
-rm -rf studence-teacher/src/main/proto
-rm -rf studence-student/src/main/proto
-rm -rf studence-attendence/src/main/proto
-rm -rf studence-management-app/src/main/proto
-cd ../studence-proto
+rm -rf studence-util/src/main/java/com/tiwari/studence/proto
+rm -rf studence-common/src/main/java/com/tiwari/studence/proto
+rm -rf studence-dynamodb-database/src/main/java/com/tiwari/studence/proto
+rm -rf studence-common-http/src/main/java/com/tiwari/studence/proto
+rm -rf studence-organisation/src/main/java/com/tiwari/studence/proto
+rm -rf studence-campus/src/main/java/com/tiwari/studence/proto
+rm -rf studence-teacher/src/main/java/com/tiwari/studence/proto
+rm -rf studence-student/src/main/java/com/tiwari/studence/proto
+rm -rf studence-attendence/src/main/java/com/tiwari/studence/proto
+rm -rf studence-login/src/main/java/com/tiwari/studence/proto
+rm -rf studence-teacher/src/main/java/com/tiwari/studence/proto
+rm -rf studence-student/src/main/java/com/tiwari/studence/proto
+rm -rf studence-classes/src/main/java/com/tiwari/studence/proto
+rm -rf studence-campus/src/main/java/com/tiwari/studence/proto
+rm -rf studence-guardian/src/main/java/com/tiwari/studence/proto
+rm -rf studence-attendence/src/main/java/com/tiwari/studence/proto
+rm -rf studence-typeAhead/src/main/java/com/tiwari/studence/proto
+rm -rf studence-management-app/src/main/java/com/tiwari/studence/proto
+cd studence-proto
 echo [COMPILEING..........]
 protoc time.proto --java_out=../studence-common/src/main/java
+protoc timePb.proto --java_out=../studence-util/src/main/java
+protoc summaryPb.proto --java_out=../studence-util/src/main/java
+protoc genericRefPb.proto --java_out=../studence-util/src/main/java
+protoc namePb.proto --java_out=../studence-util/src/main/java
+protoc searchdynamodb.proto --java_out=../studence-util/src/main/java
+protoc contactDetailsPb.proto --java_out=../studence-util/src/main/java
+protoc error.proto --java_out=../studence-util/src/main/java
 protoc requestContextPb.proto --java_out=../studence-common/src/main/java
 protoc entityPb.proto --java_out=../studence-util/src/main/java
+protoc dataTypesPb.proto --java_out=../studence-util/src/main/java
+protoc loginPb.proto --java_out=../studence-login/src/main/java
+protoc typeAheadPb.proto --java_out=../studence-typeAhead/src/main/java
 protoc organisationPb.proto --java_out=../studence-organisation/src/main/java
+protoc campusPb.proto --java_out=../studence-campus/src/main/java
+protoc teacherPb.proto --java_out=../studence-teacher/src/main/java
+protoc classPb.proto --java_out=../studence-classes/src/main/java
+protoc guardianPb.proto --java_out=../studence-guardian/src/main/java
+protoc studentPb.proto --java_out=../studence-student/src/main/java
+protoc attendencePb.proto --java_out=../studence-attendence/src/main/java
 cd ..
 echo "=================================================================================================="
 echo "************************************ Protobuf Compilation Done ***********************************"
---
+++ studence-campus/src/main/java/com/tiwari/studence/campus/indexer/CampusIndexer.java
@@ -0,0 +1,44 @@
+package com.tiwari.studence.campus.indexer;
+
+import com.tiwari.studence.common.indexer.AEntityIndexer;
+import com.tiwari.studence.proto.campus.CampusPb;
+import com.tiwari.studence.util.encoder.JsonBaseEncoderDecoder;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.HashMap;
+
+@Singleton
+public class CampusIndexer extends AEntityIndexer<CampusPb> {
+  @Inject
+  public CampusIndexer(JsonBaseEncoderDecoder jsonBaseEncoderDecoder) {
+    super(jsonBaseEncoderDecoder);
+  }
+
+  @Override
+  public HashMap<String, AttributeValue> indexer(CampusPb builder) {
+    HashMap<String, AttributeValue> items = super.indexer(builder);
+    addItemToAttritibeMap(items, CampusIndexerEnum.CAMPUS_NAME.name(),
+            builder.getName().getCanonicalName(), false, AttributeValue.Type.S,
+            CampusIndexerEnum.CAMPUS_NAME.isLowerCase());
+    addItemToAttritibeMap(items, CampusIndexerEnum.CAMPUS_MOBILE.name(),
+            builder.getContactDetails().getMobileNumbers().getPrimary().getCanonicalNumber()
+                    .toLowerCase(), false, AttributeValue.Type.S,
+            CampusIndexerEnum.CAMPUS_MOBILE.isLowerCase());
+    addItemToAttritibeMap(items, CampusIndexerEnum.CAMPUS_EMAIL.name(),
+            builder.getContactDetails().getEmails().getPrimary().getCanonicalEmail().toLowerCase(),
+            false, AttributeValue.Type.S, CampusIndexerEnum.CAMPUS_EMAIL.isLowerCase());
+    addItemToAttritibeMap(items, CampusIndexerEnum.CAMPUS_ADDRESS.name(),
+            builder.getContactDetails().getAddress().getPrimary().getCanonicalAddress()
+                    .toLowerCase(), false, AttributeValue.Type.S,
+            CampusIndexerEnum.CAMPUS_ADDRESS.isLowerCase());
+    addItemToAttritibeMap(items, CampusIndexerEnum.CAMPUS_ORG_REF_ID.name(),
+            builder.getOrganisationRef().getDbInfoId(), false, AttributeValue.Type.S,
+            CampusIndexerEnum.CAMPUS_ORG_REF_ID.isLowerCase());
+    addItemToAttritibeMap(items, CampusIndexerEnum.CAMPUS_ORG_REF_NAME.name(),
+            builder.getOrganisationRef().getName().toLowerCase(), false, AttributeValue.Type.S,
+            CampusIndexerEnum.CAMPUS_ORG_REF_NAME.isLowerCase());
+    return items;
+  }
+}
