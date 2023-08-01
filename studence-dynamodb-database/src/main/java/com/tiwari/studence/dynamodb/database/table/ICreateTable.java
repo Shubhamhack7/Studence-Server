bc1b423d85de4b0618a4402f609a54db01161085
Shubh@m_hack <shubhamtiwaricr07@gmail.com>
2023-08-01 21:20:36 +0530

    commit

--- studence-dynamodb-database/src/main/java/com/tiwari/studence/dynamodb/database/table/ICreateTable.java
+++
@@ -1,8 +0,0 @@
-package com.tiwari.studence.dynamodb.database.table;
-
-import software.amazon.awssdk.services.dynamodb.model.CreateTableResponse;
-
-public interface ICreateTable {
-	public CreateTableResponse createTable(String tableName) throws Exception;
-		
-}
