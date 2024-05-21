474ebe57beca1a997a8415746b71a647db14f843
Sundramcr07 <sundram7860@gmail.com>
2024-05-21 14:58:57 +0530

    code merge from shubhamdev
    code merge from shubhamdev

--- studence-util/src/main/java/com/tiwari/studence/proto/devices/DeviceIPAddressType.java
+++ studence-util/src/main/java/com/tiwari/studence/proto/devices/DeviceIPAddressType.java
@@ -6,7 +6,7 @@ package com.tiwari.studence.proto.devices;
 /**
  * Protobuf enum {@code com.tiwari.studence.proto.devices.DeviceIPAddressType}
  */
-public enum DeviceIPAddressType
+public enum mvnDeviceIPAddressType
     implements com.google.protobuf.ProtocolMessageEnum {
   /**
    * <code>UNKNOWN_IPADDRESS_TYPE = 0;</code>
